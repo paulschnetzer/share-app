16cfbc39465fc48cefa3c91dbdba87d6eaef4378
Paul Schnetzer <1998sheldon@gmail.com>
2020-12-03 22:38:41 +0100

    added a graphic display

--- src/App.js
+++ src/App.js
@@ -1,40 +1,86 @@
 import React, { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
 const ApiKey = 'SUN8GZHZ6UIJ6DE6';
+const requestGone =
+  'Thank you for using Alpha Vantage! Our standard API call frequency is 5 calls per minute and 500 calls per day. Please visit https://www.alphavantage.co/premium/ if you would like to target a higher API call frequency.';
+function ErrorMessage() {
+  return (
+    <p>
+      Sry you already used the 5 request per Minute.Pls wait a little bit and
+      try again!
+    </p>
+  );
+}
 function App() {
-  // const [loading, setLoading] = useState(false);
-  const [stockName, setStockName] = useState('AMZN');
+  const [stockSymbol, setStockSymbol] = useState('AMZN');
   const [rawData, setRawData] = useState(false);
-  let x = [];
-  let y = [];
+  const [searchBar, setSearchBar] = useState('A');
+  const [searchBarSuggestions, setSearchBarSuggestions] = useState(false);
+  let stockDate = [];
+  let stockPrice = [];
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
-        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockName}&outputsize=compact&apikey=${ApiKey}`,
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockSymbol}&outputsize=compact&apikey=${ApiKey}`,
       );
       setRawData(await response.json());
     };
     fetchData();
-  }, [stockName]);
+  }, [stockSymbol]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchBar}&apikey=${ApiKey}`,
+      );
+      setSearchBarSuggestions(await response.json());
+    };
+    setTimeout(function () {
+      if (searchBar) {
+        fetchData();
+      }
+    }, 500);
+  }, [searchBar]);
 
   for (let key in rawData['Time Series (Daily)']) {
-    x.push(key);
-    y.push(rawData['Time Series (Daily)'][key]['1. open']);
+    stockDate.push(key);
+    stockPrice.push(rawData['Time Series (Daily)'][key]['1. open']);
   }
 
-  if (!stockName) return <div>No data...</div>;
   return (
     <>
+      <Line
+        data={{
+          labels: stockDate,
+          datasets: [
+            {
+              label: `Stock`,
+              backgroundColor: ['rgba(75, 192, 192, 0.6)'],
+              data: stockPrice,
+            },
+          ],
+        }}
+      />
       <div>
-        <button onClick={() => setStockName('AMZN')}>Amazon</button>
-        <button onClick={() => setStockName('IBM')}>IBM</button>
-        <button onClick={() => setStockName('MSFT')}>MicroSoft</button>
+        <input onChange={(e) => setSearchBar(e.currentTarget.value)} />
       </div>
-      {x.map((date) => {
-        return <p>{date}</p>;
-      })}
-      {y.map((value) => {
-        return <p>{value}</p>;
-      })}
+      <ul>
+        {searchBarSuggestions
+          ? searchBarSuggestions.bestMatches.map((suggestion) => {
+              return (
+                <li key={suggestion['2. name']}>
+                  <button
+                    onClick={() => setStockSymbol(suggestion['1. symbol'])}
+                  >
+                    {suggestion['2. name']}
+                  </button>
+                  {}
+                </li>
+              );
+            })
+          : null}
+      </ul>
+      {rawData.Note === requestGone ? ErrorMessage() : null}
     </>
   );
 }
